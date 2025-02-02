6ca17defba36c55c7870be2eaba344a97e92c0b3
ShivaniMane <shivanimane43@gmail.com>
2025-02-02 18:53:04 +0530

    feat: add the projects with thier details

---
+++ src/components/Projects.tsx
@@ -0,0 +1,24 @@
+import { projects } from "../assets/data.tsx"
+const Projects = () => {
+  return (
+    <div className="pb-4 ">
+      <h2 className="my-20 text-center text-4xl">Projects
+      </h2>
+      <div className="">{projects.map((project) => (
+        <div key={project.id} className="mb-8 flex flex-wrap lg:justify-center ">
+          <div className="w-full lg:w-1/4">
+            <img src={project.image} alt={project.image} width={250} height={250} className="mb-6 rounded" />
+          </div>
+          <div className="w-full max-w-xl lg:w-3/4">
+            <h3 className="mb-2 font-semibold text-2xl">{project.title}</h3>
+            <p className="mb-4 text-stone-400">{project.des}</p>
+            <a href={project.deplyo_url} target="blank" rel="noopener noreferrer" >{project.deplyo_url}</a>
+            <div className="mt-3">{project.technologies.map((tech, index) => (<span key={index} className="mr-2  rounded bg-stone-900 p-2 text-sm font-medium text-stone-300">{tech}</span>))}</div>
+          </div>
+        </div>))}
+      </div>
+    </div>
+  )
+}
+
+export default Projects
