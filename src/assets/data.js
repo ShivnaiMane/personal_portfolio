d096da89279a852760edbe534e97d05e52f1b2af
ShivaniMane <shivanimane43@gmail.com>
2025-02-02 15:06:08 +0530

    remove the unncessary code and file and add other data fiels

---
+++ src/assets/data.js
@@ -0,0 +1,47 @@
+export const skills = ["React", "JavaScript", "HTML", "CSS"];
+
+export const projects = [
+  {
+    id: "1",
+    title: "Crypto Price Tracking",
+    image: "./img/crypto.jpg",
+    des: "Displays the latest prices for popular Cryptocurrencies like Bitcoin, Ethereum, etc., updated frequently. Users can select a specific Cryptocurrency to view detailed price trends and historical data. Converts Cryptocurrency prices to various fiat currencies, such as USD, EUR, etc., ensuring broader accessibility.",
+    deplyo_url: "https://crypto-place-ten-xi.vercel.app/",
+  },
+
+  {
+    id: "2",
+    title: "Music-Player",
+    image: "./img/spotify.jpg",
+    des: "Created a music streaming platform with features like user playlists, song playback, and album browsing functionality. Implemented controls for play, pause, next, and previous, ensuring a seamless user experience. Utilized Context API for managing global state, including current song, playback status, and user playlists, maintaining a smooth and responsive UI.",
+    deplyo_url: "https://music-player-weld-alpha.vercel.app/",
+  },
+  {
+    id: "3",
+    title: "BOOK APPOINTMENT",
+    image: "",
+    des: "Developed a doctor appointment platform with user registration, dynamic specialty-basedfiltering, and easy navigation between specialized doctor directories.Designed a platform where userscan view a specific doctor's appointment page, track the number of appointments booked, and access their profile with options to edit personaldetails seamlessly",
+    deplyo_url: "https://book-appointment-mu.vercel.app/",
+  },
+  {
+    id: "4",
+    title: "STREAMLINE",
+    image: "./img/netflix.jpg",
+    des: "Integrated user authentication enabling secure login and registration functionalities.Utilized Firebase Authentication to manage user accounts, ensuring secure and efficient user access and personalization Implemented on-click thumbnail video playback, providing seamless content viewing experiences",
+    deplyo_url: "https://stremline.vercel.app/",
+  },
+];
+
+export const contact = [
+  {
+    name: "linkdin",
+    icon: "",
+    url: "https://www.linkedin.com/in/shivanibmane/",
+  },
+  {
+    name: "naukri",
+    icon: "",
+    url: "https://www.naukri.com/mnjuser/profile?id=&altresid",
+  },
+  { name: "github", icon: "", url: "https://github.com/ShivnaiMane" },
+];
